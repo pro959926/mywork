187cd2c5f3d24cac39d52dbf6996addede74cd43
pro959926 <DELL@DESKTOP-4HA01RF>
2018-05-25 16:11:16 +0800

    qqq

---
+++ CloudConfigClient/src/main/java/comb/CloudClient.java
@@ -0,0 +1,11 @@
+package comb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudClient {
+    public static void main(String[] args) {
+		SpringApplication.run(CloudClient.class, args);
+	}
+}
